11cec9230812ab9eafee89454db3ae05c552a6fc
Densingmoses123 <densingmoses123@gmail.com>
2016-07-10 17:08:50 +0530

    greatest.py

---
+++ greatest.py
@@ -0,0 +1,13 @@
+a=100
+b=70
+c=50
+if(a>b):
+   if(a>c):
+       print("a is great")
+   else:
+       print("c is great")
+if(b>a):
+    if(b>c):
+        print("b is great")
+    else:
+        print("c is great")
